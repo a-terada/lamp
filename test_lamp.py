547c141901e4cede5e277731b7047ea7de22545c
Aika Terada <terada.aika@gmail.com>
2013-08-21 16:28:39 +0900

    add test code

---
+++ test_lamp.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# Testcode for LAMP
+
+__author__ = "Aika Terada"
+
+import unittest, sys
+import lamp
+
+class TestLamp(unittest.TestCase):
+	def setUp(self):
+		self.csv_file = "sample/sample_target.csv"
+		self.flag_file = "sample/sample_expression_over1.tab"
+		self.value_file = "sample/sample_expression_value.tab"
+		self.sig_level = 0.05
+
+	def testFisher(self):
+		true_k = 5; true_p = 0.00699300699301; true_comb = set(["TF1", "TF2", "TF3"])
+		enrich_lst, k, columnid2name \
+					= lamp.run( self.csv_file, self.flag_file, self.sig_level, "fisher", None, -1 )
+		
+		sys.stderr.write("check correction factor...")
+		self.assertAlmostEqual(k, true_k)
+		sys.stderr.write("\n")
+
+		sys.stderr.write("check the significance combinations...\n")
+		for comb in enrich_lst:
+			sys.stderr.write("   check combination name...")
+			detect_set = set()
+			for i in comb[0]:
+				detect_set.add( columnid2name[i-1] )
+			flag = detect_set.issubset( true_comb ) and true_comb.issubset( detect_set )
+			self.assertTrue( flag )
+			sys.stderr.write("\n")
+			sys.stderr.write("   check P-value..")
+			self.assertAlmostEqual( comb[1], true_p )
+
+	def testUTest(self):
+		true_k = 5; true_p = 0.00602414187918; true_uvalue = 45.5
+		true_comb = set(["TF1", "TF2", "TF3"])
+		enrich_lst, k, columnid2name \
+					= lamp.run( self.csv_file, self.value_file, self.sig_level, "u_test", None, -1 )
+		
+		sys.stderr.write("check correction factor...")
+		self.assertAlmostEqual(k, true_k)
+		sys.stderr.write("\n")
+
+		sys.stderr.write("check the significance combinations...\n")
+		for comb in enrich_lst:
+			sys.stderr.write("   check combination name...")
+			detect_set = set()
+			for i in comb[0]:
+				detect_set.add( columnid2name[i-1] )
+			flag = detect_set.issubset( true_comb ) and true_comb.issubset( detect_set )
+			self.assertTrue( flag )
+			sys.stderr.write("\n")
+			sys.stderr.write("   check U-value..")
+			self.assertAlmostEqual( comb[3], true_uvalue )
+			sys.stderr.write("\n")
+			sys.stderr.write("   check P-value..")
+			self.assertAlmostEqual( comb[1], true_p )
+
+if __name__ == '__main__':
+	unittest.main()
